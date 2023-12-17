4eca6639b66f1d7030c30a8874c9309a117366e0
andresdominguezruiz <andresdominguezruiz.ad@gmail.com>
2023-12-17 18:28:33 +0100

    fix:Fixing routes again
    <body>

--- app/tests/routes.py
+++ app/tests/routes.py
@@ -19,28 +19,4 @@ def show_env():
     env_vars = {key: value for key, value in os.environ.items()}
     return jsonify(env_vars)
 
-#-------------RUTAS DE TEST DE PROPOSAL-----------------------------------
-
-@test_routes.route("/test/innosoft_days/<int:id>/proposals")
-def test_get_proposals_from_innosoft_day(id):
-    state = request.args.get('state', None)
-
-    if state:
-        # Lógica para mostrar propuestas filtradas por estado
-        data_collection = Proposal.query.filter_by(innosoft_day_id=id, state=state).all()
-    else:
-        # Lógica para mostrar todas las propuestas sin filtrar por estado
-        data_collection = Proposal.query.filter_by(innosoft_day_id=id).all()
-
-    prepared_data = [{
-        'id': proposal.id,
-        'descripcion': proposal.description,
-        'tema': proposal.subject,
-        'tipo de propuesta': proposal.proposal_type.value,
-        'estado': proposal.state.value,
-        'innosoft_day_id': proposal.innosoft_day_id,
-        'usuario': User.query.get_or_404(proposal.user_id).username
-    } for proposal in data_collection]
-    return jsonify(prepared_data)
-
 
